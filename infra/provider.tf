8313c31b488567483f938fdba04e9e750540dec0
JigarMalam <jigarmalam13@gmail.com>
2025-09-13 02:57:45 +0530

    Added terraform files and remove s3 lock

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,15 +5,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "jigar-terraform-state-bucket"   # create this S3 bucket before first run
-    key            = "infra/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"             # create DynamoDB table with primary key LockID
-  }
 }
-
 provider "aws" {
   region = "ap-south-1"
 }
