13e728b0058a6c0953b724ae9e5c6ba9cdb7101e
JigarMalam <jigarmalam13@gmail.com>
2025-09-18 00:15:44 +0530

    Added key name in the main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -46,6 +46,7 @@ resource "aws_instance" "app_server" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = "Jigar_New_Key"
 
   tags = {
     Name = "${var.project_name}-server"
