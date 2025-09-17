ca5c68ede008be1cb81c4999916efc5c57a4579d
JigarMalam <jigarmalam13@gmail.com>
2025-09-18 00:57:33 +0530

    Added IGW + Route Table

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,32 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "${var.project_name}-rt"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 # Subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
